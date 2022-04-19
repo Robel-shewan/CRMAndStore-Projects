bb69a7856dcf2c60049b83e5010123983ba81521
Robel Shewangzaw <61440928+Robel-shewan@users.noreply.github.com>
2022-04-19 03:52:42 +0300

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="bg-dark">
+    <div class="container bg-dark">
+        <div class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="footer-link">Privacy policy</a><br>
+                    <a href="#" class="footer-link">Term and conditions</a><br>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Welcome to the site
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, assumenda. Culpa, libero.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Contact:
+                    </h3>
+                    <p>Phone: +251909338315</p>
+                    <p>Email: robelshewan21@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <p>copyright &copy; Robel shewangzaw - All rights reserved 2022</p>
+    </div>
+</footer>
